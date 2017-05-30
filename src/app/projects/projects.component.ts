d52e9406e8e933c27771690b0993debd321bf8b7
mentalfabric <koomjianschool@gmail.com>
2017-05-29 21:59:03 -0700

    Added functionality for looping over and displaying project items

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,22 @@
 import { Component } from "@angular/core";
 
-
+interface Projects {
+    title: string,
+    thumbnail: string,
+    description: string,
+    url: string
+}
 
 @Component ({
     selector: "pt-projects",
     templateUrl: "./projects.component.html"
 })
 
-export class ProjectsComponent {}
+export class ProjectsComponent {
+    projectItems : Projects[] = [{
+        title: "Mennonite Insurance Company Web Page",
+        thumbnail: "placeholder",
+        description: "lorem lorem lorem",
+        url: "mennoniteinsurance.com"
+    }]
+}
